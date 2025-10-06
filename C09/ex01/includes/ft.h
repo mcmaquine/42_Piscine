fee6a2206619429cd5244304c704dbd460e0d979
mcmaquine <mcmaquine@gmail.com>
2025-10-06 10:56:43 -0300

    added pdfs

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 17:40:47 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/06/30 17:42:35 by mmaquine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
---
+++ C09/ex01/main.c
@@ -0,0 +1,8 @@
+#include "includes/ft.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[0]);
+	ft_putstr("\n");
+}
